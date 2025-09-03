2dfdbc6ce7181c0cb28c0eacc7ab433ee6cfb7e6
davewil <davewil1973@gmail.com>
2025-09-03 13:18:01 +0100

    _

--- lib/kuma_san_kanji_web/live/stroke_order_events.ex
+++ lib/kuma_san_kanji_web/live/stroke_order_events.ex
@@ -1,10 +1,11 @@
 defmodule KumaSanKanjiWeb.StrokeOrderEvents do
   import Phoenix.LiveView, only: [push_event: 3]
+  import Phoenix.Component, only: [assign: 3]
 
   @stroke_events ["stroke_order_restart", "stroke_order_step", "stroke_order_toggle_style"]
 
   def toggle(socket) do
-    Phoenix.LiveView.assign(socket, :show_stroke_order, !socket.assigns.show_stroke_order)
+    assign(socket, :show_stroke_order, !socket.assigns.show_stroke_order)
   end
 
   def stroke_event?(event), do: event in @stroke_events
